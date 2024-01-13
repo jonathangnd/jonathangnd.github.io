a3456f19fb751367e9a646a1874508878eaa7bd3
BuildTools <unconfigured@null.spigotmc.org>
2024-01-13 18:18:57 +0100

    Commit for Mac

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
-function getName ()
+//
+const pageName = document.getElementById('pagename');
+function getPageName ()
 {
-    let n =window.location.pathname
-    n = n.split('/')
-    n = n[n.length - 1]
+    let n =window.location.pathname;
+    n = n.split('/');
+    n = n[n.length - 1];    
     n = n.substr(0, n.lastIndexOf("."));
+    n = n.replace(new RegExp("(%20|_|-)", "g"), " ");
+
 
     switch (n) {
         case 'index':
-            n = ' acceuil '
+            n = 'Acceuil'
+            break;
+        case 'skills':
+            n = 'Compétences'
             break;
     }
+
+    n1 = n.charAt(0).toUpperCase() //Premiere Lettre
+    n = n.substring(1) //Retirer la premiere lettre
+    n = (n1 + n)
     return n;
 }
+function changePageTitle () 
+{
+    let t = document.title;
+    let n = getPageName();
+    
+    document.title = `${n} - ${t}`;
+}
 
-const pageName = document.getElementById('pagename');
-pageName.textContent = getName()
+//
+changePageTitle()
+pageName.textContent = getPageName();
